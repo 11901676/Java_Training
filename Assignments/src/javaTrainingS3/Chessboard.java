158c0b7c541cc345fe38864f10274f1c70447bef
Dipesh Tiwari <dipesh.11901676@lpu.in>
2022-01-21 04:51:36 +0530

    This is the assignment in which we have to place 4 Queens by taking the inputs from user in such a manner that only one queen can reside in a single column and row.

--- Assignments/src/javaTrainingS3/Chessboard.java
+++ Assignments/src/javaTrainingS3/Chessboard.java
@@ -32,6 +32,8 @@ public class Chessboard {
 			
 			for(int i = 0;i < 8;i++)
 			{
+				
+				//Checking the necessary condition for rows and column place of queen
 				if((row1==row2||row1==row3||row1==row4||row2==row3||row2==row4||row3==row4)||(column1==column2||column1==column3||column1==column4||column2==column3||column2==column4||column3==column4))
 				{
 						System.out.println("More than one queens are not allowed in a single row or column");
@@ -39,11 +41,13 @@ public class Chessboard {
 				}
 				for(int j = 0;j < 8;j++)
 				{
+					
+					//If user enters valid inputs then for user's inputs replace the boxes with queens
 					if((i == row1 && j == column1)||(i == row2 && j == column2)||(i == row3 && j == column3)||(i == row4 && j == column4))
 					{
 						System.out.print(queen+" ");
 					}
-					// Otherwise print boxes
+					//Otherwise print boxes
 					else if(i%2==0)
 					{
 						char square = ( (j+1) % 2 == 0 )? blackSquare : whiteSquare;
