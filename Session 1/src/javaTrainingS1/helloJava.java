a7311eb1e7a8e8dd2785456a6c5d2fe40e2f6b3c
Dipesh Tiwari <dipesh.11901676@lpu.in>
2022-01-21 00:10:04 +0530

    Just Started exploring Java

---
+++ Session 1/src/javaTrainingS1/helloJava.java
@@ -0,0 +1,14 @@
+package javaTrainingS1;
+
+public class helloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have started my beutiful journey of learning and exploring Java");
+		System.out.println("Thanks to Ishant Sir for being my instructor in this lovely journey with his lovley smilies :)");
+		System.out.println("I will try my best to make this training fruitful as much as i can!!");
+
+	}
+
+}
